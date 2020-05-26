23eb2a5a0b5ed21fe4dfe7b7a37aac1ddab70d1b
Hector Morales <tuntu99@gmail.com>
2020-05-25 23:56:39 -0600

    Esta es la version final del xilofono

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,52 @@
+import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  void playSound(int soundNumber) {
+    //solicitamos como parametro el numero de nota
+    final player = AudioCache();
+    player.play(
+        'note$soundNumber.wav'); //recibimos como un int en un input el nnumero
+  }
+
+  Expanded buildKey({int soundNumber, Color color}) {
+    return Expanded(
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(18),
+        child: Container(
+          margin: EdgeInsets.all(2),
+          child: FlatButton(
+            color: color,
+            onPressed: () {
+              playSound(
+                  soundNumber); //parametro: numero de nota que recibe la funicon
+            },
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
-          child: Container(),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              buildKey(soundNumber: 1, color: Colors.red),
+              buildKey(soundNumber: 2, color: Colors.orange),
+              buildKey(soundNumber: 3, color: Colors.yellow),
+              buildKey(soundNumber: 4, color: Colors.green),
+              buildKey(soundNumber: 5, color: Colors.teal),
+              buildKey(soundNumber: 6, color: Colors.blue),
+              buildKey(soundNumber: 7, color: Colors.purple),
+            ],
+          ),
         ),
       ),
     );
